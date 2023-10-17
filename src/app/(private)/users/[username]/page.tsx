4e5991fddac44b11678fcbcadabffc4132079606
Henrick <henrickdeguzman@gmail.com>
2023-10-17 15:08:12 +0800

    Create table action drop down menu reusable component

---
+++ src/app/(private)/users/[username]/page.tsx
@@ -0,0 +1,3 @@
+export default function UserDetailPage() {
+  return <div>User detail page</div>;
+}
--- src/app/(private)/users/roles/columns.tsx
+++ src/app/(private)/users/roles/columns.tsx
@@ -1,70 +1,59 @@
-'use client';
+"use client";
 
-import { ColumnDef } from '@tanstack/react-table';
-import { getRoles } from './actions';
-import { formatDate } from '@/utils/dates';
-import { ArrowRight, MoreHorizontal } from 'lucide-react';
-import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/navigation';
+import { ColumnDef } from "@tanstack/react-table";
+import { getRoles } from "./actions";
+import { formatDate } from "@/utils/dates";
+import { ArrowRight } from "lucide-react";
+import { useRouter } from "next/navigation";
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
+import TableActions from "@/components/table/TableActions";
 
-export type Role = Awaited<ReturnType<typeof getRoles>>['roles'][number];
+export type Role = Awaited<ReturnType<typeof getRoles>>["roles"][number];
 
 export function useRolesTableColumns() {
   const router = useRouter();
 
   const columns: ColumnDef<Role>[] = [
     {
-      accessorKey: 'name',
-      header: 'Name',
+      accessorKey: "name",
+      header: "Name",
       cell: ({ row }) => (
-        <span className='capitalize'>{row.original.name}</span>
+        <span className="capitalize">{row.original.name}</span>
       ),
     },
     {
-      accessorKey: 'createdAt',
-      header: 'Created at',
+      accessorKey: "createdAt",
+      header: "Created at",
       cell: ({ row }) => {
         const date = row.original.createdAt as unknown as string;
         return formatDate(date);
       },
     },
     {
-      accessorKey: 'updatedAt',
-      header: 'Modified at',
+      accessorKey: "updatedAt",
+      header: "Modified at",
       cell: ({ row }) => {
         const date = row.original.updatedAt as unknown as string;
         return formatDate(date);
       },
     },
     {
-      id: 'actions',
+      id: "actions",
       cell: ({ row }) => {
         return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant='ghost' className='h-8 w-8 p-0'>
-                <MoreHorizontal className='h-4 w-4' />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end'>
-              <DropdownMenuLabel>Actions</DropdownMenuLabel>
-              <DropdownMenuItem
-                className='flex items-center gap-1'
-                onClick={() => router.push(`/users/roles/${row.original.name}`)}
-              >
-                View Permissions
-                <ArrowRight size={16} />
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <TableActions
+            title="Actions"
+            items={[
+              {
+                key: 1,
+                label: "View Permissions",
+                onClick() {
+                  router.push(`/users/roles/${row.original.name}`);
+                },
+                icon: <ArrowRight size={16} />,
+              },
+            ]}
+          />
         );
       },
     },
